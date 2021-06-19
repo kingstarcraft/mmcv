5b9cff59db4178c39755510b1ddf5320fa391ea1
zero <kingstarcraft@foxmail.com>
2021-06-20 01:13:00 +0800

    fix resume bug on windows.

--- mmcv/utils/config.py
+++ mmcv/utils/config.py
@@ -1,5 +1,6 @@
 # Copyright (c) Open-MMLab. All rights reserved.
 import ast
+import os
 import os.path as osp
 import platform
 import shutil
@@ -276,11 +277,12 @@ class Config:
             warnings.warn(
                 'Please check "file_format", the file format may be .py')
 
-        with tempfile.NamedTemporaryFile('w', suffix=file_format) as temp_file:
+        with tempfile.NamedTemporaryFile('w', suffix=file_format, delete=False) as temp_file:
             temp_file.write(cfg_str)
             temp_file.flush()
             temp_file.close()
             cfg = Config.fromfile(temp_file.name)
+            os.remove(temp_file.name)
         return cfg
 
     @staticmethod
