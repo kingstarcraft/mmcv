ab7cb687beba27bd31f49c53cb0c141ad12b227f
zero <kingstarcraft@foxmail.com>
2022-01-30 23:01:17 +0800

    fix bug with evalution

--- mmcv/runner/hooks/evaluation.py
+++ mmcv/runner/hooks/evaluation.py
@@ -384,7 +384,7 @@ class EvalHook(Hook):
                 # infer from eval_results
                 self._init_rule(self.rule, list(eval_res.keys())[0])
             if self.key_indicator not in eval_res:
-                return eval_res[self.key_indicator]
+                return None
             return eval_res[self.key_indicator]
 
         return None
